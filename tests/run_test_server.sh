854a48eacf57701c28839f5c075e85c72be19d02
Claas Nieslony <github@nieslony.at>
2026-01-28 23:07:41 +0100

    Dont't hardcode python version

--- tests/run_test_server.sh
+++ tests/run_test_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ROOT=../root
 
-PYTHONPATH=$ROOT/usr/local/lib/python3.13/site-packages/ \
+
+ROOT="$(realpath $(dirname $0) )/../root"
+PYTHON_VERSION=$( python --version | awk '{ gsub("\\.[0-9]+$", "", $2); print $2; }' )
+
+PYTHONPATH=$ROOT/usr/local/lib/python$PYTHON_VERSION/site-packages/ \
     $ROOT/usr/local/bin/arachne-dbus --bus session --console-log --directory /tmp/openvpn-server
